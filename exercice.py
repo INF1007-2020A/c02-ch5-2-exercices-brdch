6404cf1ac1ba60adb54ddf9ad0adbbde8a31a9be
amine <amine.bourdache@polymtl.ca>
2020-09-24 18:36:20 -0400

    cours 7

--- exercice.py
+++ exercice.py
@@ -4,21 +4,44 @@
 import math
 
 def get_num_letters(text):
-	return 0
+	num_letters = 0
+	#pour le nombre de lettre ds texte ci dessous
+	for chr in text:
+		num_letters += int(chr.isalnum())
+	return num_letters
 
 def get_word_length_histogram(text):
-	return [0]
+	histogram = [0]
+	for word in text.split():
+		length = get_num_letters(word)
+		if length >= len(histogram):
+			histogram += [0] * (length - len(histogram) + 1)
+		histogram[length] += int(length != 0)
+
+	return histogram
+
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
+	alignment = len(str(len(histogram) - 1))
+	return "\n".join([f"{i : >{alignment}} {ROW_CHAR * elem}" for i, elem in enumerate(histogram) if i != 0])
+	# result = ""
+	# for i, elem in enumerate(histogram[1:]):
+	# 	#lespace apres le : est pr dire de remplir avec des espaces (4> alignes sur 4 caractere voir rep) le faire avec alignment au lieu de 4
+	# 	result += f"{i : >4}" + "\n"
 
-	return ""
+	# return result
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
+	height = max(histogram)
+	result = ""
+	for i in range(height - 1, -1, -1):
+		result += "".join([BLOCK_CHAR if elem >= i + 1 else " " for elem in histogram[1:]]) + "\n"
+	result += LINE_CHAR * len(histogram)
 
-	return ""
+	return result
 
 
 if __name__ == "__main__":
